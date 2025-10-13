d33d61565a97a47da6417a6171686fab4915a542
Shivang-Soni <soni.shivang.999@gmail.com>
2025-10-13 15:26:58 +0530

    Aktualisiert keyvault.py: Erstellung einer AzureKeyVaultClient Klasse

---
+++ app/clients/keyvault.py
@@ -0,0 +1,41 @@
+import os
+import logging
+from app.config import AZURE_KEY_VAULT_URL
+from azure.identity import DefaultAzureCredential
+from azure.keyvault.secrets import SecretClient
+
+# ============================================
+# Logging Konfiguration
+# ============================================
+logging.basicConfig(logging.INFO)
+
+class AzureKeyVaultClient:
+    """
+    Azure Key Vault Client zum sicheren Abrufen sensibler Secrets.
+    """
+
+    def __init__(self):
+        self.key_vault_url = AZURE_KEY_VAULT_URL
+        if not self.key_vault_url:
+            raise ValueError(
+                "AZURE_KEY_VAULT_URL nicht in der .env Datei gesetzt."
+                )
+        
+        # Verwendung von Azure Default Credential
+        #  (beispielsweise Managed Identity, CLI Login)
+        self.credential = DefaultAzureCredential()
+        self.client = SecretClient(
+            vault_url=self.key_vault_url,
+            credential=self.credential
+            )
+
+    def get_secret(self, secret_name: str) -> str:
+        """
+        Ruft den Wert aus Azure Key Vault ab
+        """
+        try: 
+            secret = self.client.get_secret(secret_name)
+            return secret.value
+        except Exception as e:
+            logging.error("Fehler beim Aufruf vom Geheimnis: {secret_name}")
+            return None
