ab2e43111ce0037a714cbb3d081b01fabdb8ad0b
Shivang-Soni <soni.shivang.999@gmail.com>
2025-10-17 23:13:06 +0530

    Verfeinert App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,37 @@
 import React from "react";
-import ChatBot from "./ChatBot"
+import ChatBot from "./ChatBot";
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-center text-2xl font-bold my-4">Azure Chatbot Lösung</h1>
-      <ChatBot/ >
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        backgroundImage: "url('/background.jpg')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        padding: "20px",
+      }}
+    >
+      <h1
+        style={{
+          color: "black",
+          textShadow: "1px 1px 4px rgba(0,0,0,0.7)",
+          fontSize: "2rem",
+          marginBottom: "20px",
+        }}
+      >
+        Azure Generative KI Assistent
+      </h1>
+      <div style={{ width: "400px", flex: 1 }}>
+        <ChatBot />
+      </div>
     </div>
   );
-};
+}
 
 export default App;
