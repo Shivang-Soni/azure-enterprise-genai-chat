7889c6e65b1025be2ffe5c32c2c5308d661d2646
Shivang-Soni <soni.shivang.999@gmail.com>
2025-10-19 20:40:50 +0530

    Finaler Commit

--- app/services/upload_documents.py
+++ app/services/upload_documents.py
@@ -6,7 +6,7 @@ import logging
 
 from azure.search.documents import SearchClient
 from azure.core.credentials import AzureKeyCredential
-from config import AZURE_SEARCH_ENDPOINT, AZURE_SEARCH_INDEX, AZURE_SEARCH_KEY
+from app.config import AZURE_SEARCH_ENDPOINT, AZURE_SEARCH_INDEX, AZURE_SEARCH_KEY
 
 
 # Logger Konfiguration
@@ -21,14 +21,22 @@ search_client = SearchClient(
 )
 
 # Dokumente aus der JSON Datei importieren
-with open("docs.json", "r", encoding="utf-8") as f:
+file_name = "docs_info"
+with open(f"app/data/{file_name}.json", "r", encoding="utf-8") as f:
     docs = json.load(f)
-
+print(docs)
 # Wenn vorhanden, hochladen
 if docs:
     result = search_client.upload_documents(documents=docs)
+    erfolgreich = True
     for r in result:
         if not r.succeeded:
             logger.warning(f"Dokument wurde nicht hochgeladen: {r.key}.")
+            erfolgreich = False
+
 else:
     logger.error("Dokumente konnten nicht gefunden werden.")
+    erfolgreich = False
+
+if erfolgreich:
+    logger.info("Alle Dateien wurden erfolgreich hochgeladen.")
